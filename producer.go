3ec9564320d4a55105d23f85d7c10f2c2471d137
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2024-04-17 11:03:12 -0300

    feat: Se crea un loop infinito para una ves estableida una coneccion recivir varios paquetes

--- producer.go
+++ producer.go
@@ -20,34 +20,42 @@ func main() {
 	}
 	defer conn.Close()
 
-	// XML de ejemplo
-	xmlData := `<counter vltid="7805" date="2023/12/19 14:00:00" denom="1" ci="49842000" co="47216500" dr="14110376" jp="0" cc="11484876" hpcc="78831000" jj="53306" jg="29535" pa="352" pwr="19" cxb="11208000" tci="0" tco="0" hl="0" ca="0" ti="2902376" to="32653876" mpeb="0" apeb="0" app="0" mpp="0" tkiqty="48" tkoqty="2068" capr="0" state="0"/>`
-
-	// Crear el mensaje con el tamaño del cuerpo
-	message := generateMessageToSend(xmlData)
-
-	fmt.Println("Enviando ", message.Header, message.Body)
-
-	// Convertir el tamaño del cuerpo a bytes y enviarlo como header
-
-	// headerBuf := make([]byte, 1)
-	headerBuf := make([]byte, 2)
-	// headerBuf[0] = message.Header
-	binary.BigEndian.PutUint16(headerBuf, message.Header)
-	_, err = conn.Write(headerBuf)
-	if err != nil {
-		fmt.Println("Error al enviar el header:", err)
-		return
+	// XMLs de ejemplo
+	xmls := []string{
+		`<counter vltid="7805" date="2023/12/19 14:00:00" denom="1" ci="49842000" co="47216500" dr="14110376" jp="0" cc="11484876" hpcc="78831000" jj="53306" jg="29535" pa="352" pwr="19" cxb="11208000" tci="0" tco="0" hl="0" ca="0" ti="2902376" to="32653876" mpeb="0" apeb="0" app="0" mpp="0" tkiqty="48" tkoqty="2068" capr="0" state="0"/>`,
+		`<counter vltid="7806" date="2024/12/19 15:00:00" denom="1" ci="49842001" co="47216501" dr="14110377" jp="0" jj="53307" to="32653877" mpeb="1" apeb="1" app="1" mpp="1" tkiqty="49" tkoqty="2069" capr="1" state="1"/>`,
+		`<counter vltid="7807" date="2024/12/20 16:00:00" denom="1" ci="49842001" co="47216501" dr="14110377" jp="0" jj="53307" to="32653877" apeb="1" app="1" mpp="1" tkiqty="49" tkoqty="2069" capr="1" state="1"/>`,
 	}
 
-	// Enviar el cuerpo (XML)
-	_, err = conn.Write([]byte(message.Body))
-	if err != nil {
-		fmt.Println("Error al enviar el cuerpo:", err)
-		return
+	for _, xmlData := range xmls {
+
+		// Crear el mensaje con el tamaño del cuerpo
+		message := generateMessageToSend(xmlData)
+
+		fmt.Println("Enviando ", message.Header, message.Body)
+
+		// Convertir el tamaño del cuerpo a bytes y enviarlo como header
+
+		// headerBuf := make([]byte, 1)
+		headerBuf := make([]byte, 2)
+		// headerBuf[0] = message.Header
+		binary.BigEndian.PutUint16(headerBuf, message.Header)
+		_, err = conn.Write(headerBuf)
+		if err != nil {
+			fmt.Println("Error al enviar el header:", err)
+			return
+		}
+
+		// Enviar el cuerpo (XML)
+		_, err = conn.Write([]byte(message.Body))
+		if err != nil {
+			fmt.Println("Error al enviar el cuerpo:", err)
+			return
+		}
+
+		fmt.Println("Mensaje enviado correctamente.")
 	}
 
-	fmt.Println("Mensaje enviado correctamente.")
 }
 
 func generateMessageToSend(data string) Message {
--- server.go
+++ server.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	//	"encoding/binary"
 	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 const BUFFER_SIZE = 32
-
-type Message struct {
-	Header uint16
-	Body   string
-}
 
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
@@ -38,38 +32,43 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	// Leer el encabezado
-	headerBuf := make([]byte, 2)
-	_, err := conn.Read(headerBuf)
-	if err != nil {
-		fmt.Println("Error al leer el encabezado:", err)
-		return
-	}
+	ciclo := 0
 
-	// bodySize := uint8(header[0])
-	bodySize := binary.BigEndian.Uint16(headerBuf)
-
-	// Leer el cuerpo en fragmentos del tamaño adecuado
-	var body []byte
-	for len(body) < int(bodySize) {
-		remaining := int(bodySize) - len(body)
-
-		toRead := min(remaining, BUFFER_SIZE)
-		buf := make([]byte, toRead)
-		n, err := conn.Read(buf)
-
-		fmt.Printf("bodySize %v, BUFFER_SIZE %v, toRead %v , n %v, remaining %v | %v \n", bodySize, BUFFER_SIZE, toRead, n, remaining, string(buf))
-
+	for {
+		ciclo++
+		// Leer el encabezado
+		headerBuf := make([]byte, 2)
+		_, err := conn.Read(headerBuf)
 		if err != nil {
-			fmt.Println("Error al leer el cuerpo:", err)
+			fmt.Println("Error al leer el encabezado:", err)
 			return
 		}
-		body = append(body, buf[:n]...)
-	}
 
-	// Procesar el cuerpo (XML en este caso)
-	xmlData := string(body)
-	fmt.Println("XML recibido:", xmlData)
+		// bodySize := uint8(header[0])
+		bodySize := binary.BigEndian.Uint16(headerBuf)
+
+		// Leer el cuerpo en fragmentos del tamaño adecuado
+		var body []byte
+		for len(body) < int(bodySize) {
+			remaining := int(bodySize) - len(body)
+
+			toRead := min(remaining, BUFFER_SIZE)
+			buf := make([]byte, toRead)
+			n, err := conn.Read(buf)
+
+			fmt.Printf("  bodySize %v, BUFFER_SIZE %v, toRead %v , n %v, remaining %v | %v \n", bodySize, BUFFER_SIZE, toRead, n, remaining, string(buf))
+
+			if err != nil {
+				fmt.Println("Error al leer el cuerpo:", err)
+				return
+			}
+			body = append(body, buf[:n]...)
+		}
+
+		// Procesar el cuerpo (XML en este caso)
+		xmlData := string(body)
+		fmt.Println("XML recibido:", ciclo, xmlData)
+	}
 }
 
 func min(a, b int) int {
